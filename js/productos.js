08c53071cb8059968ba2c982dde7804de30295d6
Thiago Dalfino <tdalfino20@gmail.com>
2022-08-11 18:35:12 -0300

    segunda preentrega

---
+++ js/productos.js
@@ -0,0 +1,28 @@
+const productos = [
+    {id: 1, tipo: 'Procesador', nombre: 'Intel i3', precio: 17000, foto: './assets/i3.jpg'},
+    {id: 2, tipo: 'Procesador', nombre: 'Intel i5', precio: 30000, foto: './assets/i5.jpg'},
+    {id: 3, tipo: 'Procesador', nombre: 'Intel i7', precio: 65000, foto: './assets/i7.jpg'},
+    {id: 4, tipo: 'Procesador', nombre: 'Intel i9', precio: 110000, foto: './assets/i9.jpg'},
+    {id: 5, tipo: 'Procesador', nombre: 'Ryzen 3', precio: 20000, foto: './assets/ryzen 3.jpg'},
+    {id: 6, tipo: 'Procesador', nombre: 'Ryzen 5', precio: 43000, foto: './assets/ryzen 5.jpg'},
+    {id: 7, tipo: 'Procesador', nombre: 'Ryzen 7', precio: 63000, foto: './assets/ryzen 7.jpg'},
+    {id: 8, tipo: 'Procesador', nombre: 'Ryzen 9', precio: 100000, foto: './assets/ryzen 9.jpg'},
+    {id: 9, tipo: 'Grafica', nombre: '"RTX 2060', precio: 95000, foto: './assets/rtx2060.jpg'},
+    {id: 10, tipo: 'Grafica', nombre: '"RTX 2070', precio: 180000, foto: './assets/rtx2070.jpg'},
+    {id: 11, tipo: 'Grafica', nombre: '"RTX 2080', precio: 260000, foto: './assets/rtx2080.jpg'},
+    {id: 12, tipo: 'Mother', nombre: 'Asus B450m Am4', precio: 17500, foto: './assets/am4.jpg'},
+    {id: 13, tipo: 'Mother', nombre: 'Msi B450m Pro-m2', precio: 20000, foto: './assets/msi.jpg'},
+    {id: 14, tipo: 'RAM', nombre: 'HyperX 4GB ddr4', precio: 5700, foto: './assets/4gb.jpg'},
+    {id: 15, tipo: 'RAM', nombre: 'HyperX 8GB ddr4', precio: 7600, foto: './assets/8gb.jpg'},
+    {id: 16, tipo: 'RAM', nombre: 'HyperX 16GB ddr4', precio: 18700, foto: './assets/16gb.jpg'},
+    {id: 17, tipo: 'Almacenamiento', nombre: 'HDD WD 1TB', precio: 10000, foto: './assets/1tb.jpg'},
+    {id: 18, tipo: 'Almacenamiento', nombre: 'SSD KINGSTON 240GB', precio: 9500, foto: './assets/240gb.jpg'},
+    {id: 19, tipo: 'Almacenamiento', nombre: 'SSD KINGSTON 480GB', precio: 5700, foto: './assets/480gb.jpg'},
+    {id: 20, tipo: 'Almacenamiento', nombre: 'SSD KINGSTON 250GB m.2', precio: 6300, foto: './assets/m.2.jpg'},
+    {id: 21, tipo: 'Fuente', nombre: '500w 80plus bronze', precio: 9400, foto: './assets/500w.jpg'},
+    {id: 22, tipo: 'Fuente', nombre: '750w 80plus gold', precio: 22000, foto: './assets/750w.jpg'},
+    {id: 23, tipo: 'Fuente', nombre: '1000w 80plus gold', precio: 33000, foto: './assets/1000w.jpg'}
+
+
+
+]
